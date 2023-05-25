382f823622ff97e3227e402f18cb05d9be1149c8
Nikhitha1809 <chilukuri.nikhitha1999@gmail.com>
2023-05-25 18:02:16 +0530

    25/05/2023

--- frontend/src/components/Cards/cards.js
+++ frontend/src/components/Cards/cards.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './cards.css';
+// import './cards.css';
 const Cards = () => {
   const [userData, setUserData] = useState([]);
 
@@ -19,26 +19,42 @@ const Cards = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Users Data:</h1>
-      
+    <div className='total-container'>
+      <h1>User Data:</h1>
+
       <ul className='ul-container'>
-      <div>
-        {userData.map(user => (
-          <li key={user._id} >
-            <div  className='card-container'>
-            <p>Name: {user.firstname} {user.lastname}</p>
-            <p>Email: {user.email}</p>
-            <p>Dob: {user.dob}</p>
-            <p>Phone: {user.phone}</p>
-            <img src={user.photo} alt="pic" className='image'/>
-            </div>
-          </li>
-        ))}
+        <div>
+          {userData.map(user => (
+            <li key={user._id} >
+              <div className='card-container'>
+                <div className='card'>
+                  <p><b>Name: </b>
+                    {user.firstname} {user.lastname}</p>
+                </div>
+                <div className='card'>
+                  <p><b>Email: </b>{user.email} </p>
+                </div>
+                <div className='card'>
+                  <p><b>Dob: </b>{user.dob}</p>
+                </div>
+                <div className='card'>
+                  <p><b>Phone: </b>{user.phone}</p>
+                </div>
+                <div className='card'>
+                  <p><b>Sign: </b></p>
+                  <img src={user.signature} alt='sign' className='image' />
+                </div>
+                <div className='card'>
+                  <p><b>Photo: </b></p>
+                  <img src={user.image} alt="pic" className='image' />
+                </div>
+              </div>
+            </li>
+          ))}
         </div>
       </ul>
-      
-    </div>
+
+      </div>
   );
 };
 
